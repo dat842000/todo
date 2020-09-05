9baad5b92943fc394bd6085e1bb5637c77ab2b4e
Anh Quan Nguyen <nguyen.anhquan@gmail.com>
2020-09-05 11:40:11 +0200

    FIX EMOJI role

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ function App() {
                 </block>
             </div>
 
-            <h3>Copyright &copy; 2020 mytodo <span>😀</span></h3>
+            <h3>Copyright &copy; 2020 mytodo <span role={"img"}>😀</span></h3>
         </>
     );
 }
