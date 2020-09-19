ea19a6c8647ee74e7c9c7a62463b0965e548beda
Anh Quan Nguyen <nguyen.anhquan@gmail.com>
2020-09-19 08:41:21 +0200

    FIX Clock

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Typography from "@material-ui/core/Typography";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import IconButton from "@material-ui/core/IconButton";
@@ -8,84 +8,71 @@ import MenuIcon from '@material-ui/icons/Menu';
 import InputBase from "@material-ui/core/InputBase";
 import SearchIcon from '@material-ui/icons/Search';
 import {fade} from "@material-ui/core";
+import Clock from "./Clock";
 
 // Bai tap 3: hien thi dong ho he thong (build <Clock>)
 //           - Hien thi <Button> "GO TO TODAY" de nhay ngay cua calendar den ngay hien tai
 
+// tao JSS = CSS in Javascript
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,    // camelCase
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
 
 function AppHeader({
                        handelSearch,
                        search
                    }) {
-    let time = new Date().toLocaleTimeString();
-
-
-
-    const [ctime, setCtime] = useState(time)
-
-    const UpdateTime = () => {
-        time = new Date().toLocaleTimeString();
-        setCtime(time);
-    }
-    setInterval(UpdateTime, 1000);
-
-
-
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            flexGrow: 1,
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-        title: {
-            flexGrow: 1,
-            display: 'none',
-            [theme.breakpoints.up('sm')]: {
-                display: 'block',
-            },
-        },
-        search: {
-            position: 'relative',
-            borderRadius: theme.shape.borderRadius,
-            backgroundColor: fade(theme.palette.common.white, 0.15),
-            '&:hover': {
-                backgroundColor: fade(theme.palette.common.white, 0.25),
-            },
-            marginLeft: 0,
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                marginLeft: theme.spacing(1),
-                width: 'auto',
-            },
-        },
-        searchIcon: {
-            padding: theme.spacing(0, 2),
-            height: '100%',
-            position: 'absolute',
-            pointerEvents: 'none',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        inputRoot: {
-            color: 'inherit',
-        },
-        inputInput: {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                width: '12ch',
-                '&:focus': {
-                    width: '20ch',
-                },
-            },
-        },
-    }));
-
     const classes = useStyles();
 
     return (
@@ -101,7 +88,7 @@ function AppHeader({
                         <MenuIcon />
                     </IconButton>
                     <Typography className={classes.title} variant="h6" noWrap>
-                        TODOs List {ctime}
+                        TODOs List <Clock/>
                     </Typography>
                     <div className={classes.search}>
                         <div className={classes.searchIcon}>
---
+++ src/components/Clock.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import {format} from "date-fns";
+import useInterval from "../hooks/useInterval";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+function formatTime(t) {
+    return format(t, "dd.MM.yyyy HH:mm:ss.SSS");
+}
+
+const useStyles = makeStyles((theme) => {
+    return {
+        clock: {
+            color: theme.palette.warning.dark
+        }
+    }
+});
+
+function Clock() {
+    const classes = useStyles();
+    const [ctime, setCtime] = useState(new Date());
+
+    useInterval(() => {
+        setCtime(new Date());
+    }, 1);
+    return (
+        <>
+            <Typography variant={"body2"} component={"span"} className={classes.clock}>
+                {formatTime(ctime)}
+            </Typography>
+        </>
+    );
+}
+
+export default Clock;
